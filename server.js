b9ec357266f9888e053eb03aa482bb336f381968
root <root@ip-172-31-91-236.ec2.internal>
2023-04-21 10:38:14 +0000

    Added my Project

---
+++ server.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const express = require('express');
+
+//constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+//APP
+const app = express();
+app.get('/', (req, res) => {
+	res.send('Hello World');
+});
+
+app.listen(PORT, HOST, () => {
+	console.log(`Running on http://${HOST}:${PORT}`);
+});
+
+
